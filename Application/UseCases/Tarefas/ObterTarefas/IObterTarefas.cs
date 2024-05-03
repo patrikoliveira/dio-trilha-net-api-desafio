ffbe41e2196c9162b88319cab13486461921fac7
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 19:59:11 -0300

    feat: caso de uso obter todas as tarefas

---
+++ Application/UseCases/Tarefas/ObterTarefas/IObterTarefas.cs
@@ -0,0 +1,10 @@
+﻿using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefas
+{
+    public interface IObterTarefas
+    {
+        public Task<IList<RespostaTarefaJson>> Execute();
+    }
+}
+
---
+++ Application/UseCases/Tarefas/ObterTarefas/ObterTarefas.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefas
+{
+    public class ObterTarefas : IObterTarefas
+    {
+        private readonly ITarefaReadOnlyRepository readOnlyRepository;
+        private readonly IMapper mapper;
+
+        public ObterTarefas(ITarefaReadOnlyRepository readOnlyRepository, IMapper mapper)
+        {
+            this.readOnlyRepository = readOnlyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<RespostaTarefaJson>> Execute()
+        {
+            var tarefas = await readOnlyRepository.GetAll();
+            return mapper.Map<IList<RespostaTarefaJson>>(tarefas);
+        }
+    }
+}
+
