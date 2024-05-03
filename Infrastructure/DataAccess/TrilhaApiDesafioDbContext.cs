d54bcb3402d440d0f609081b3519b958365a3f5e
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 19:49:55 -0300

    refactor: refatorando o contexto para camada de dados em infra

---
+++ Infrastructure/DataAccess/TrilhaApiDesafioDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Domain.Entities;
+
+namespace TrilhaApiDesafio.Infrastructure.DataAccess
+{
+    public class TrilhaApiDesafioDbContext : DbContext
+    {
+        public TrilhaApiDesafioDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
+
