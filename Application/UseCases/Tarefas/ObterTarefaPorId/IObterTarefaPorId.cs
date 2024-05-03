c62da42646dcd3bea669e9f08640ef96ebc2c0a5
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 19:59:41 -0300

    feat: caso de uso obter tarefa por id

---
+++ Application/UseCases/Tarefas/ObterTarefaPorId/IObterTarefaPorId.cs
@@ -0,0 +1,10 @@
+﻿using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorId
+{
+    public interface IObterTarefaPorId
+    {
+        public Task<RespostaTarefaJson> Execute(int id);
+    }
+}
+
---
+++ Application/UseCases/Tarefas/ObterTarefaPorId/ObterTarefaPorId.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+using TrilhaApiDesafio.Shared.Exceptions.ExceptionsBase;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorId
+{
+    public class ObterTarefaPorId : IObterTarefaPorId
+    {
+        private readonly ITarefaReadOnlyRepository readOnlyRepository;
+        private readonly IMapper mapper;
+
+        public ObterTarefaPorId(ITarefaReadOnlyRepository readOnlyRepository, IMapper mapper)
+        {
+            this.readOnlyRepository = readOnlyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<RespostaTarefaJson> Execute(int id)
+        {
+            var tarefa = await readOnlyRepository.GetById(id) ?? throw new EntityNotFoundException(new List<string>()
+                {
+                    $"Tarefa de id: {id} não encontrada na base de dados"
+                });
+
+            return mapper.Map<RespostaTarefaJson>(tarefa);
+        }
+    }
+}
+
