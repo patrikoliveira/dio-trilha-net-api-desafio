b6ec3d1a33cf17332232f9c10451204eeb6b175e
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 19:55:26 -0300

    feat: mapeamentos para request-response

---
+++ Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Application.UseCases.Tarefas.AtualizarTarefa;
+using TrilhaApiDesafio.Application.UseCases.Tarefas.CriarTarefa;
+using TrilhaApiDesafio.Domain.Entities;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.Services.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToDomain();
+            DomainToResponse();
+        }
+
+        private void RequestToDomain()
+        {
+            CreateMap<CriarTarefaRequest, Tarefa>()
+                .ForMember(dest => dest.Status, opt => opt.Ignore());
+
+            CreateMap<AtualizarTarefaRequest, Tarefa>();
+        }
+
+        private void DomainToResponse()
+        {
+            CreateMap<Tarefa, RespostaTarefaJson>();
+        }
+    }
+}
+
