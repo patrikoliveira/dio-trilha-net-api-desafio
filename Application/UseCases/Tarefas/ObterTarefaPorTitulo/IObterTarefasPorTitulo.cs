227aeb87173668065d0e1928925e93bd1b7059e4
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 20:00:39 -0300

    feat: caso de uso obter tarefa por titulo

---
+++ Application/UseCases/Tarefas/ObterTarefaPorTitulo/IObterTarefasPorTitulo.cs
@@ -0,0 +1,10 @@
+﻿using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorTitulo
+{
+    public interface IObterTarefasPorTitulo
+    {
+        public Task<IList<RespostaTarefaJson>> Execute(string titulo);
+    }
+}
+
---
+++ Application/UseCases/Tarefas/ObterTarefaPorTitulo/ObterTarefasPorTitulo.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorTitulo
+{
+    public class ObterTarefasPorTitulo : IObterTarefasPorTitulo
+    {
+        private readonly ITarefaReadOnlyRepository readOnlyRepository;
+        private readonly IMapper mapper;
+
+        public ObterTarefasPorTitulo(ITarefaReadOnlyRepository readOnlyRepository, IMapper mapper)
+        {
+            this.readOnlyRepository = readOnlyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<RespostaTarefaJson>> Execute(string titulo)
+        {
+            var tarefas = await readOnlyRepository.GetByTitulo(titulo);
+
+            return mapper.Map<IList<RespostaTarefaJson>>(tarefas);
+        }
+    }
+}
+
