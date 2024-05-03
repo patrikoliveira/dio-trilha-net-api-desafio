f8ca31fc8535d6cef0410a827fd034b46e5bc113
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 20:00:27 -0300

    feat: caso de uso obter tarefa por status

---
+++ Application/UseCases/Tarefas/ObterTarefaPorStatus/IObterTarefasPorStatus.cs
@@ -0,0 +1,11 @@
+﻿using TrilhaApiDesafio.Domain.Entities;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorStatus
+{
+    public interface IObterTarefasPorStatus
+    {
+        public Task<IList<RespostaTarefaJson>> Execute(EnumStatusTarefa statusTarefa);
+    }
+}
+
---
+++ Application/UseCases/Tarefas/ObterTarefaPorStatus/ObterTarefasPorStatus.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Domain.Entities;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorStatus
+{
+    public class ObterTarefasPorStatus : IObterTarefasPorStatus
+    {
+        private readonly ITarefaReadOnlyRepository readOnlyRepository;
+        private readonly IMapper mapper;
+
+        public ObterTarefasPorStatus(ITarefaReadOnlyRepository readOnlyRepository, IMapper mapper)
+        {
+            this.readOnlyRepository = readOnlyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<RespostaTarefaJson>> Execute(EnumStatusTarefa statusTarefa)
+        {
+            var tarefas = await readOnlyRepository.GetByStatus(statusTarefa);
+            return mapper.Map<IList<RespostaTarefaJson>>(tarefas);
+        }
+    }
+}
+
