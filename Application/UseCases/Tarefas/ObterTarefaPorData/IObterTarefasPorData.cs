fea4d276353e4e223621d11cc7660316b45df23c
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 20:00:12 -0300

    feat: caso de uso obter tarefa por data

---
+++ Application/UseCases/Tarefas/ObterTarefaPorData/IObterTarefasPorData.cs
@@ -0,0 +1,10 @@
+﻿using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorData
+{
+    public interface IObterTarefasPorData
+    {
+        public Task<IList<RespostaTarefaJson>> Execute(DateTime data);
+    }
+}
+
---
+++ Application/UseCases/Tarefas/ObterTarefaPorData/ObterTarefasPorData.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+using TrilhaApiDesafio.Shared.Comunication.Responses;
+
+namespace TrilhaApiDesafio.Application.UseCases.Tarefas.ObterTarefaPorData
+{
+    public class ObterTarefasPorData : IObterTarefasPorData
+    {
+        private readonly ITarefaReadOnlyRepository readOnlyRepository;
+        private readonly IMapper mapper;
+
+        public ObterTarefasPorData(ITarefaReadOnlyRepository readOnlyRepository, IMapper mapper)
+        {
+            this.readOnlyRepository = readOnlyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IList<RespostaTarefaJson>> Execute(DateTime data)
+        {
+            var tarefas = await readOnlyRepository.GetByData(data);
+
+            return mapper.Map<IList<RespostaTarefaJson>>(tarefas);
+        }
+    }
+}
+
