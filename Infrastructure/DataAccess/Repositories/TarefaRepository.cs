96864f10404d4266b6b41c7698faeab5db588eec
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-05-03 19:51:11 -0300

    feat: implementa repository de tarefas + unit of work

---
+++ Infrastructure/DataAccess/Repositories/TarefaRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Domain.Entities;
+using TrilhaApiDesafio.Domain.Repositories.Tarefa;
+
+namespace TrilhaApiDesafio.Infrastructure.DataAccess.Repositories
+{
+    public class TarefaRepository : ITarefaWriteOnlyRepository, ITarefaReadOnlyRepository
+    {
+        private readonly TrilhaApiDesafioDbContext dbContext;
+        public TarefaRepository(TrilhaApiDesafioDbContext dbContext) => this.dbContext = dbContext;
+
+        public async Task Add(Tarefa tarefa) => await this.dbContext.AddAsync(tarefa);
+
+        public async Task Delete(int id)
+        {
+            var tarefa = await dbContext.Tarefas.FirstAsync(c => c.Id == id);
+            dbContext.Tarefas.Remove(tarefa);
+        }
+
+        public void Update(Tarefa tarefa) => dbContext.Tarefas.Update(tarefa);
+
+        public async Task<IList<Tarefa>> GetAll() => await dbContext.Tarefas.AsNoTracking().ToListAsync();
+
+        public async Task<IList<Tarefa>> GetByData(DateTime data) => await dbContext.Tarefas.Where(c => c.Data.Date == data.Date).ToListAsync();
+
+        public async Task<Tarefa> GetById(int id) => await dbContext.Tarefas.FirstOrDefaultAsync(c => c.Id == id);
+
+        public async Task<IList<Tarefa>> GetByStatus(EnumStatusTarefa status) => await dbContext.Tarefas.Where(c => c.Status == status).ToListAsync();
+
+        public async Task<IList<Tarefa>> GetByTitulo(string titulo) => await dbContext.Tarefas.Where(c => c.Titulo == titulo).ToListAsync();
+    }
+}
+
---
+++ Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using TrilhaApiDesafio.Domain.Repositories;
+
+namespace TrilhaApiDesafio.Infrastructure.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TrilhaApiDesafioDbContext dbContext;
+
+        public UnitOfWork(TrilhaApiDesafioDbContext dbContext) => this.dbContext = dbContext;
+
+        public async Task Commit() => await this.dbContext.SaveChangesAsync();
+    }
+}
+
